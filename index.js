f3ef377d7326af44bdaf1ebe840a673eb48f6fee
ItsGickow <riskijah5@gmail.com>
2024-11-12 20:22:22 +0800

    Add Modals

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ app.use(compression({
         return compression.filter(req, res);
     }
 }));
-app.use(function (req, res, next) {
+
+app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header(
         'Access-Control-Allow-Headers',
@@ -23,15 +24,15 @@ app.use(function (req, res, next) {
     next();
 });
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(rateLimiter({ windowMs: 15 * 60 * 1000, max: 100, headers: true }));
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
     console.log(req.method, req.url);
     next();
 });
 app.use(express.json());
+app.use(rateLimiter({ windowMs: 15 * 60 * 1000, max: 100, headers: true }));
 
 app.post('/player/login/dashboard', (req, res) => {
-    res.status(302).redirect('/player/growid/login/validate');
+    res.sendFile(__dirname + '/public/html/dashboard.html');
 });
 
 app.all('/player/growid/login/validate', (req, res) => {
@@ -48,14 +49,14 @@ app.all('/player/growid/login/validate', (req, res) => {
     );
 });
 
-app.post('/player/validate/close', function (req, res) {
+app.post('/player/validate/close', function(req, res) {
     res.send('<script>window.close();</script>');
 });
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
     res.send('Hello World!');
 });
 
-app.listen(5000, function () {
+app.listen(5000, function() {
     console.log('Listening on port 5000');
 });
